72e5981b4cb6c260689a5e3ffb4a3fdaa52ca2a7
Jindřich Pergler <jindrich@pergler.eu>
2025-02-12 15:17:38 +0100

    Add UseCaseExecutor

--- src/Jiper.Infrastructure.Handlers/UseCases.cs
+++ src/Jiper.Infrastructure.Handlers/UseCases.cs
@@ -8,23 +8,36 @@ public interface IUseCase : IRequest
 
 public interface IUseCase<out TResponse>
     : IRequest<TResponse>
-    where TResponse : IUseCaseResponse
 {
 }
 
-public interface IUseCaseResponse
-{
-}
-
-public interface IUseCaseHandler<TUseCase>
+public interface IUseCaseHandler<in TUseCase>
     : IRequestHandler<TUseCase>
     where TUseCase : IUseCase
 {
 }
 
-public interface IUseCaseHandler<TUseCase, TResponse>
+public interface IUseCaseHandler<in TUseCase, TResponse>
     : IRequestHandler<TUseCase, TResponse>
     where TUseCase : IUseCase<TResponse>
-    where TResponse : IUseCaseResponse
 {
+}
+
+public interface IUseCaseExecutor
+{
+    Task Execute(IUseCase useCase, CancellationToken ct = default);
+    Task<TResult> Execute<TResult>(IUseCase<TResult> useCase, CancellationToken ct = default);
+}
+
+public class UseCaseExecutor(IMediator mediator) : IUseCaseExecutor
+{
+    public async Task Execute(IUseCase useCase, CancellationToken ct = default)
+    {
+        await mediator.Send(useCase, ct);
+    }
+
+    public async Task<TResult> Execute<TResult>(IUseCase<TResult> useCase, CancellationToken ct = default)
+    {
+        return await mediator.Send(useCase, ct);
+    }
 }
